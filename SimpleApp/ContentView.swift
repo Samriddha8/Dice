36ede220b70130f24454f9a02980fdb8c619b0f1
Samriddha <samriddhajungrayamajhi@gmail.com>
2022-03-18 12:38:27 -0400

    initial commit

---
+++ SimpleApp/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  SimpleApp
+//
+//  Created by Samriddha Rayamajhi on 3/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView{
+            VStack(alignment: .leading, spacing: 16){
+                ForEach(0 ..< 40){num in
+                    HStack{
+                        Text("My number \(num)")
+                        Spacer()
+                    }
+                }
+            }//: VStack
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Samriddha Rayamajhi on 3/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
